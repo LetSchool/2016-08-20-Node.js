471b3a259828feaef1c4d1a9fe5afa90a363baaf
cfsghost <cfsghost@gmail.com>
2016-08-20 07:07:15 +0000

    example to require sub folder

--- module/entry.js
+++ module/entry.js
@@ -1,6 +1,10 @@
 var a = require('./a');
 var b = require('./b');
+var c = require('./c');
+var d = require('./c/d');
 
 console.log('Entry');
 console.log(a);
 console.log(b());
+console.log(c);
+console.log(d);
