932c421359485b25d8dd155f48be2df67cd755da
cfsghost <cfsghost@gmail.com>
2016-08-20 06:48:44 +0000

    added module examples

---
+++ module/b.js
@@ -0,0 +1,5 @@
+module.exports = function() {
+  console.log('B');
+
+  return 'B!';
+};
---
+++ module/entry.js
@@ -0,0 +1,6 @@
+var a = require('./a');
+var b = require('./b');
+
+console.log('Entry');
+console.log(a);
+console.log(b());
