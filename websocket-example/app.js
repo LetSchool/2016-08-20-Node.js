c73c1fa8478a0fa5818e144c00db22f306a1a4d2
cfsghost <cfsghost@gmail.com>
2016-08-21 07:22:47 +0000

    make it can talk to each other

--- websocket-example/app.js
+++ websocket-example/app.js
@@ -2,18 +2,26 @@ var koa = require('koa');
 var serve = require('koa-static');
 var Router = require('koa-router');
 var websockify = require('koa-websocket');
+var events = require('events');
 
 var app = koa();
 var socket = websockify(app);
 
 app.use(serve('./public'));
 
+var eventEmitter = new events.EventEmitter();
 
 var wsRouter = new Router();
 wsRouter.get('/', function *() {
+  var self = this;
+
   this.websocket.send('Welcome');
   this.websocket.on('message', function(msg) {
-    console.log(msg);
+    eventEmitter.emit('update', msg);
+  });
+
+  eventEmitter.on('update', function(message) {
+    self.websocket.send(message);
   });
 });
 
