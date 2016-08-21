c75e518645a9e37c3bab6a3615b424dc0ea6d985
cfsghost <cfsghost@gmail.com>
2016-08-21 03:27:18 +0000

    added template example

---
+++ koa-views-example/app.js
@@ -0,0 +1,20 @@
+var koa = require('koa');
+var Router = require('koa-router');
+var views = require('koa-views');
+
+var app = koa();
+var router = new Router();
+
+app.use(views(__dirname + '/views', {
+  extension: 'pug',
+  map: {
+    html: 'pug'
+  }
+}));
+
+router.get('/', function *() {
+  yield this.render('index');
+});
+
+app.use(router.middleware());
+app.listen(3000);
