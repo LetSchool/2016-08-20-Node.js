8bbbdaf6538b49a41fc2e8506f21be0e44898cc0
cfsghost <cfsghost@gmail.com>
2016-08-21 07:30:47 +0000

    auto remove listener from event emitter

--- websocket-example/app.js
+++ websocket-example/app.js
@@ -15,14 +15,21 @@ var wsRouter = new Router();
 wsRouter.get('/', function *() {
   var self = this;
 
+  function pushMessage(message) {
+    self.websocket.send(message);
+  }
+
   this.websocket.send('Welcome');
   this.websocket.on('message', function(msg) {
     eventEmitter.emit('update', msg);
   });
 
-  eventEmitter.on('update', function(message) {
-    self.websocket.send(message);
+  this.websocket.on('close', function() {
+    console.log('disconnect');
+    eventEmitter.removeListener('update', pushMessage);
   });
+
+  eventEmitter.on('update', pushMessage);
 });
 
 app.ws
